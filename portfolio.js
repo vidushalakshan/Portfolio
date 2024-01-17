f5312bf622a448a91d6cdeaeb4489a7276d62767
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-01-18 01:47:24 +0530

    aded navbar

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:589fa8c7f461645c44addf849d93258262ce871d004c75dba53adff9ab630a76
-size 194
+oid sha256:1f979c733dd57834155c06c8a09ce3f866ce27eecdd90f411a078f45a496a63a
+size 200
