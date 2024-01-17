7c36ce5019175d5023d2507813290910eb681813
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-01-18 00:11:22 +0530

    aded home background

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,3 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:589fa8c7f461645c44addf849d93258262ce871d004c75dba53adff9ab630a76
+size 194
